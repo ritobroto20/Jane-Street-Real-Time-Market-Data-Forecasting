8ecbfed7917ce57a411b499e60fc17ba46ff9c5a
Ritobroto Mohanta <ritobrotom@examroom.ai>
2025-02-02 13:24:04 +0530

    EDA updated

---
+++ program.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    int a=75; 
+    int b=1;
+    int z;
+    printf("%d",z);
+    if (z==a+b){
+        printf("Hello");
+    }
+    else
+    {
+        printf("Byee");
+    }
+}
